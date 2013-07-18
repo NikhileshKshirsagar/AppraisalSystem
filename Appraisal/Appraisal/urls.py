2c0172623f27b501efb421037d440d3b8cc4895b
Nihkilesh Kshirsagar <nikhilesh.kshirsagar@soft-corner.com>
2013-07-18 11:56:54 +0530

    Added libraries
    1. Added libraries jquery, jquery-ui, bootstrap, font-awesome.
    2. Changes to create user page to show errors and added css.

--- Appraisal/Appraisal/urls.py
+++ Appraisal/Appraisal/urls.py
@@ -17,3 +17,17 @@ urlpatterns = patterns('Login',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 )
+
+urlpatterns += patterns('UserProfile',
+    # Examples:
+    url(r'^CreateUser/$', 'views.CreateUser', name='CreateUser'),
+     
+     url(r'^logout/$', 'views.logout'),
+    # url(r'^Appraisal/', include('Appraisal.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
