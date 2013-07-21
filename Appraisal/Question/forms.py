8e2f7cd99be1d2945dc137b745e8ece990e5ec70
amarkamthe <amar.kamthe@soft-corner.com>
2013-07-21 23:57:36 +0530

    UI design for creating questions
    Added view, form and CreateQuestion html files
    It contains UI design for accepting questions of MCQ and subjective type
    questions
    
    Also added changes in welcome screen.

--- Appraisal/Question/forms.py
+++ Appraisal/Question/forms.py
@@ -1,8 +1,20 @@
 from django import forms
-from Login.models import Question
+from Login.models import UserDetails, Question
+
 class QuestionForm(forms.ModelForm):
- 
+    questionType=(
+              ('select' , '--SELECT--'),
+              ('MCQ' , 'MCQ'),
+              ('Subjective' , 'Subjective')
+         )
+    objquestion = forms.CharField(error_messages={'required': 'Please enter question'}, widget=forms.Textarea(attrs={'rows':4}),label="Question")
+    objinfo = forms.CharField(widget=forms.Textarea(attrs={'rows':4}), label="Additional information (optional)")
+    objintent = forms.CharField(error_messages={'required': 'Please select intent'}, widget=forms.TextInput(attrs={'class':'tableRow span4 search-query'}),label="Intent")
+    objlevel = forms.CharField(label="Question level")
+    objweight = forms.CharField(label="Question weight")
+    objtype = forms.ChoiceField(choices=questionType, error_messages={'required': 'Please select question type'},widget=forms.Select(attrs={'class':'tableRow span4 search-query', 'style': 'border-radius: 15px 15px 15px 15px;'}),label="Question type")
+        
     class Meta:
         model=Question
-        fields=("question","level","weight","type","info","intent")
+        fields=("objquestion","objlevel","objweight","objtype","objinfo","objintent")
     
