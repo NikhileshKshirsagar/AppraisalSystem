faffc58bd5b51aa4643b1b420a262f119e3425ee
Nikhilesh Kshirsagar <kshirsagar.nikhilesh@gmail.com>
2013-07-22 08:07:39 +0530

    Validation in create user. Added UserProfile structure.
    1. Implemented save and clean methods.
    2. Added userProfile template and forms.

--- Appraisal/UserProfile/UserProfileForms.py
+++ Appraisal/UserProfile/UserProfileForms.py
@@ -5,8 +5,9 @@ Created on 17-Jul-2013
 '''
 
 from django import forms
-from Login.models import UserDetails
+from Login.models import UserDetails, UserAttributes, Language, UserAttributes
 from django.utils import timezone
+from cProfile import label
 
 class UserCreate(forms.ModelForm):
     usertype=(
@@ -14,13 +15,49 @@ class UserCreate(forms.ModelForm):
               ('Administrator' , 'Administrator'),
               ('Employee' , 'Employee')
          )
-    firstname = forms.CharField(error_messages={'required': 'Please enter user first name'}, widget=forms.TextInput(attrs={'class':'tableRow span4 search-query'}))
-    lastname = forms.CharField(error_messages={'required': 'Please enter user last name'}, widget=forms.TextInput(attrs={'class':'tableRow span4 search-query'}))
-    emailid = forms.CharField(error_messages={'required': 'Please enter email id'}, widget=forms.TextInput(attrs={'class':'tableRow span4 search-query'}))
+    firstname = forms.CharField(label='First name', error_messages={'required': 'Please enter user first name'}, widget=forms.TextInput(attrs={'class':'tableRow span5 search-query'}))
+    lastname = forms.CharField(label='Last name',error_messages={'required': 'Please enter user last name'}, widget=forms.TextInput(attrs={'class':'tableRow span5 search-query'}))
+    emailid = forms.CharField(label='Email address',error_messages={'required': 'Please enter email id'}, widget=forms.TextInput(attrs={'class':'tableRow span5 search-query'}))
     user_level = forms.CharField()
     user_weight = forms.CharField()
-    type = forms.ChoiceField(choices=usertype, error_messages={'required': 'Please select user type'},widget=forms.Select(attrs={'class':'tableRow span4 search-query', 'style': 'border-radius: 15px 15px 15px 15px;'}))
+    type = forms.ChoiceField(label='User type',choices=usertype, error_messages={'required': 'Please select user type'},widget=forms.Select(attrs={'class':'tableRow span5 search-query', 'style': 'border-radius: 15px 15px 15px 15px;'}))
     
+    def save(self,  userId,commit=True):
+        obj_userForm = super(UserCreate, self).save(commit=False)
+        print "Form values"
+        print self.data['emailid']
+        obj_userForm.emailid = self.data['emailid']
+        obj_userForm.user_level = self.data['user_level']
+        obj_userForm.user_weight = self.data['user_weight']
+        obj_userForm.type = self.data['type']
+        obj_userForm.modified_by = userId
+        obj_userForm.modified_on = timezone.now()
+        obj_userForm.save()
+    
+    def clean(self):
+        #data = self.cleaned_data
+        print "Inside clean"
+        print self.cleaned_data['type']
+        if self.cleaned_data['type'] == "select":
+            self.fields['type'].error_messages["errorUserType"]  = "Please select user type"
+            raise forms.ValidationError(self.fields['type'].error_messages["errorUserType"])
+        return self.cleaned_data
     class Meta():
         model=UserDetails
         fields = ('firstname','lastname','emailid','user_level', 'user_weight', 'type',)
+        
+class UserProfile_UserDetailForm(forms.ModelForm):
+    class Meta():
+        model=UserDetails
+        fields = ('username','password',)
+    
+class UserProfile_LanguageForm:
+    newLanguage = forms.CharField(label=("Add new language"), required=False)
+    choices = [(obj_language.language_id, obj_language.language) for obj_language in Language.objects.all()]
+    language = forms.ChoiceField(choices = choices)
+    class Meta():
+        model=UserAttributes
+        fields = ('tech_working','tech_known','tech_willing','language_working','language_known','language_willing')
+        
+#class UserProfile_DesignationForm:
+    
--- Appraisal/UserProfile/views.py
+++ Appraisal/UserProfile/views.py
@@ -14,27 +14,35 @@ def CreateUser(request):
     if request.method == 'POST':
         userCreateform = UserCreate(request.POST)
         print "POST request"
-        if UserDetails.objects.get(emailid=request.POST['emailid']).emailid ==  request.POST['emailid']:     
+        try:
+            s_existingEmail = UserDetails.objects.get(emailid=request.POST['emailid']).emailid
+        except UserDetails.DoesNotExist:
+           s_existingEmail = ''
+               
+        if s_existingEmail != '' and s_existingEmail ==  request.POST['emailid']:     
                 print "in else"
                 return render_to_response('Userprofile/CreateUser.html', {'errorEmail' : 'Email id already exists', 'userCreateform' : userCreateform});
         else:    
             if userCreateform.is_valid():
+                #userCreateform.clean_userType()
                 print "Form valid"
-                obj_user = userCreateform.save(commit=False)
+                i_UserId = UserDetails.objects.get(user_id=request.session['UserID']).user_id
+                userCreateform.save(commit=False, userId = i_UserId)
                 print "SESSION - USER ID : "
                 print request.session['UserID']
-                obj_user.emailid = request.POST['emailid']
-                obj_user.user_level = request.POST['user_level']
-                obj_user.user_weight = request.POST['user_weight']
-                obj_user.type = request.POST['type']
-                obj_user.modified_by = UserDetails.objects.get(user_id=request.session['UserID']).user_id
-                obj_user.modified_on = timezone.now()
-                obj_user.save()
-                return render_to_response('Userprofile/CreateUser.html', {'successMsg' : 'User created successfully', 'userCreateform' : userCreateform});
+                
+                # redirect to next page
+                return render_to_response('Welcome.html', {'successMsg' : 'User created successfully', 'userCreateform' : userCreateform});
             else:
+                #userCreateform.clean_type()
                 print UserDetails.objects.get(user_id=request.session['UserID']).firstname
                 print "Invalid form"
                 return render_to_response('Userprofile/CreateUser.html', { 'userCreateform' : userCreateform }, context_instance = RequestContext( request))
     else:
         userCreateform = UserCreate()
         return render_to_response('Userprofile/CreateUser.html', { 'userCreateform' : userCreateform }, context_instance = RequestContext( request))
+    
+#def UserProfile(request):
+    #if request.method == 'POST':
+        
+     
