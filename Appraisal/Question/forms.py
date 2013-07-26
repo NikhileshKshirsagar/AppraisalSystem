fbd3fe9a33cb5eca6bc53ce8ec009df6d2aefa11
amarkamthe <amar.kamthe@soft-corner.com>
2013-07-26 18:32:54 +0530

    Question creation with option
    Added code to add the option to type MCQ to database
    Added code to add subjective type question which will not have the
    options.
    Made changes in templates, form and views.

--- Appraisal/Question/forms.py
+++ Appraisal/Question/forms.py
@@ -1,20 +1,70 @@
 from django import forms
-from Login.models import UserDetails, Question
+from Login.models import UserDetails, Question, Option, OptionHeader
+from django.utils import timezone
 
 class QuestionForm(forms.ModelForm):
     questionType=(
-              ('select' , '--SELECT--'),
-              ('MCQ' , 'MCQ'),
-              ('Subjective' , 'Subjective')
+              ('1' , 'Subjective'),
+              ('2' , 'MCQ')
          )
-    objquestion = forms.CharField(error_messages={'required': 'Please enter question'}, widget=forms.Textarea(attrs={'rows':4,'class':'span4'}),label="Question")
-    objinfo = forms.CharField(widget=forms.Textarea(attrs={'rows':4,'class':'span4'}), label="Additional information (optional)")
-    objintent = forms.CharField(error_messages={'required': 'Please select intent'}, widget=forms.TextInput(attrs={'class':'tableRow span4 search-query'}),label="Intent")
-    objlevel = forms.CharField(label="Question level")
-    objweight = forms.CharField(label="Question weight")
-    objtype = forms.ChoiceField(choices=questionType, error_messages={'required': 'Please select question type'},widget=forms.Select(attrs={'class':'tableRow span4 search-query', 'style': 'border-radius: 15px 15px 15px 15px;'}),label="Question type")
-        
+    intentType =(
+                 ('0','--SELECT--'),
+                 ('1','+ve'),
+                 ('2','-ve')
+                )
+    question = forms.CharField(error_messages={'required': 'Please enter question'}, widget=forms.Textarea(attrs={'rows':4,'class':'span4'}),label="Question")
+    info = forms.CharField(required=False, widget=forms.Textarea(attrs={'rows':4,'class':'span4'}), label="Additional information (optional)")
+    intent = forms.ChoiceField(choices=intentType, error_messages={'required': 'Please select intent'}, widget=forms.Select(attrs={'class':'tableRow span4 search-query', 'style': 'border-radius: 15px 15px 15px 15px;'}),label="Intent")
+    level = forms.CharField(required=False,label="Question level")
+    weight = forms.CharField(required=False,label="Question weight")
+    type = forms.ChoiceField(choices=questionType, error_messages={'required': 'Please select question type'},widget=forms.Select(attrs={'class':'tableRow span4 search-query', 'style': 'border-radius: 15px 15px 15px 15px;'}),label="Question type")
+    category=forms.CharField(required=False)
+    type_text=forms.CharField(required=False,widget=forms.HiddenInput())
     class Meta:
         model=Question
-        fields=("objquestion","objlevel","objweight","objtype","objinfo","objintent")
+        fields=("question","level","weight","type","info","intent","type_text")
+        
+    def clean_weight(self):
+        if self.cleaned_data['weight'] == '0':
+            raise forms.ValidationError("Please select weight of question.")
+        return self.cleaned_data['weight']
     
+    def clean_level(self):
+        if self.cleaned_data['level'] == '0':
+            raise forms.ValidationError("Please select level of question.")
+        return self.cleaned_data['level']
+    
+    def save(self,userId, commit=True):
+        objQuestionForm = super(QuestionForm, self).save(commit=False)
+        objQuestionForm.question = self.data['question']
+        objQuestionForm.level = self.data['level']
+        objQuestionForm.intent = self.data['intent']
+        objQuestionForm.weight = self.data['weight']
+        objQuestionForm.type = self.data['type']
+        objQuestionForm.category = ''#self.data['category']
+        objQuestionForm.option_header = OptionHeader.objects.latest('option_header_id')
+        objQuestionForm.info = self.data['info']
+        objQuestionForm.modified_by = userId
+        objQuestionForm.modified_on = timezone.now()
+        objQuestionForm.save() 
+
+
+class OptionFrom(forms.ModelForm):
+    option_header_text = forms.CharField(label="Option header",error_messages={'required':'Enter Option header'})
+    option_text = forms.CharField(label="Options",error_messages={'required':'Enter Options'})
+    option_headerid = forms.CharField(required=False,widget=forms.HiddenInput(),initial=0)
+    
+    class Meta:
+        model=Option
+        fields=("option_text","option_header_text")
+        
+    def save(self, userId,commit=True ):
+         objOptionFrom = super(OptionFrom, self).save(commit=False)
+         objOptionHeader = OptionHeader.objects.create(title=self.data['option_header_text'], modified_by=userId, modified_on=timezone.now())
+         objOptionFrom.option_header = OptionHeader.objects.latest('option_header_id')
+         objOptionFrom.option_text = self.data['option_text']
+         objOptionFrom.order = 1
+         objOptionFrom.modified_by = userId
+         objOptionFrom.modified_on = timezone.now()
+         objOptionFrom.save()
+         
--- Appraisal/Question/views.py
+++ Appraisal/Question/views.py
@@ -2,23 +2,46 @@
 from django.shortcuts import render_to_response
 from django.contrib.sessions.models import Session
 
-from forms import QuestionForm
+from forms import QuestionForm, OptionFrom
 from django.http import HttpResponseRedirect 
 from django.core.context_processors import csrf
 
+from Login.models import UserDetails
 def questionCreateView(request):
     
     args={}
     args.update(csrf(request))
     args['error']=''
+    objQuestionForm = QuestionForm(request.POST or None)
+    objOptionForm = OptionFrom(request.POST or None)
     if request.method == 'POST':
-        objQuestionForm = QuestionForm(request.POST)
-        if objQuestionForm.is_valid():
-                print "Form valid"
+        if request.POST['type'] == '2':
+            if objQuestionForm.is_valid() and objOptionForm.is_valid():
+                flag=True
+            else:
+                flag=False
         else:
-             args['questionCreateform']=objQuestionForm
-             return render_to_response('Questions/CreateQuestion.html',args)
+            objOptionForm = OptionFrom()
+            if objQuestionForm.is_valid():
+                flag=True   
+            else:
+                flag=False
+             
+        if flag == True: 
+           i_UserId = UserDetails.objects.get(user_id=request.session['UserID'])
+           objOptionForm.save(commit=False, userId=i_UserId)
+           objQuestionForm.save(commit=False, userId = i_UserId)
+           args['successMsg']="Question created successfully"
+           objOptionForm = OptionFrom()
+           objQuestionForm = QuestionForm()
+           args['questionCreateform']=objQuestionForm
+           args['optionCreateform']=objOptionForm
+           return render_to_response('Questions/CreateQuestion.html',args)
+        else:
+           args['questionCreateform']=objQuestionForm
+           args['optionCreateform']=objOptionForm
+           return render_to_response('Questions/CreateQuestion.html',args)
     else:
-        objQuestionForm = QuestionForm()
         args['questionCreateform']=objQuestionForm
+        args['optionCreateform']=objOptionForm
         return render_to_response('Questions/CreateQuestion.html',args)
