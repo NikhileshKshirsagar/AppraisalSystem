98e40d34fff7e84faa6cd7fdc0d7a6440be79ff2
amarkamthe <amar.kamthe@soft-corner.com>
2013-07-29 14:35:29 +0530

    slider added change function
    Added change function to change the value when slider with some value is
    called.

--- Appraisal/Static_Files/js/CommonComponents.js
+++ Appraisal/Static_Files/js/CommonComponents.js
@@ -7,17 +7,17 @@ function LoadSlider(sliderElement, sliderValueElement, maxValue, minValue, defau
 			 max: maxValue,
 			 value: defaultValue,
 			 slide: function( event, ui ) {
-			 $( sliderValueElement ).val( ui.value );
+			 	$( sliderValueElement ).val( ui.value );
+			 },
+			 change:function(event,ui){
+			 	$( sliderValueElement ).val( ui.value );
 			 }
 			 });
-			 $( sliderValueElement ).val( $( sliderElement ).slider( "value" ) );	
-	});
-	
+	});	
 }
 
-function setSliderValue(sliderElement, sliderValueElement, sliderValue){
+function setSliderValue(sliderElement, sliderValue){
 	$(function(){
 		$( sliderElement ).slider('value', sliderValue);
-		$( sliderValueElement ).val( sliderValue );
 	});
 }
