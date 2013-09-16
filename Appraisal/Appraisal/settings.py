9dedfbd7bec1024c0ffadcc6993a2a0bd5b6a118
Nikhilesh <nick@Megatrix.(none)>
2013-09-16 19:11:41 +0530

    Removing unwanted apps

--- Appraisal/Appraisal/settings.py
+++ Appraisal/Appraisal/settings.py
@@ -116,10 +116,6 @@ TEMPLATE_DIRS = (
 )
 
 INSTALLED_APPS = (
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'UserProfile'
